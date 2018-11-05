77b7c16ece5d9bf6d93aab86d211a0263c7a98f8
Lucas de Castro Bertasso <lucashellsing99@hotmail.com>
2018-11-05 17:36:58 -0200

    Lucas
    Object

---
+++ ObjectPooling/Assets/Scripts/CubeSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawn : MonoBehaviour {
+
+    ObjectPooling objectPooling;
+
+	// Use this for initialization
+	private void Start () {
+
+        objectPooling = ObjectPooling.Instance;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        objectPooling.SpawnFromPool("Cube", transform.position, Quaternion.identity);
+		
+	}
+}
